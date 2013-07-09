791814d7309de07a99acb04c1150f2c77f8d8b3a
Den Lipovsky <dennis.lipovsky@gmail.com>
2013-07-09 07:46:06 -0400

    refactoring the board and introducing color

--- src/ton_eighty/views/practice.clj
+++ src/ton_eighty/views/practice.clj
@@ -12,41 +12,31 @@
     [:label {:class "control-label"} "Hit"]
     [:div {:class "controls btn-group"} (map button [1 2 3])]]))
 
-(defn- board-section-button [cnt number]
+(defn- board-section-button [cnt number cssClass]
   [:div {:class "row"}
    [:div {:class "span1"}
-    [:button (str cnt number)]]])
+    [:button {:class cssClass} (str cnt number)]]])
 
-(defn- board-section [number]
-  [:div {:class (str "section-" number)}
-   (board-section-button "D" number)
-   (board-section-button "F" number)
-   (board-section-button "T" number)
-   (board-section-button "S" number)])
+(defn- board-section [number cssClass]
+  [:div {:class (str "section-" number " " cssClass)}
+   (board-section-button "D" number (str cssClass " small"))
+   (board-section-button "F" number (str cssClass " big"))
+   (board-section-button "T" number (str cssClass " small"))
+   (board-section-button "S" number (str cssClass " big"))])
+
+(defn- board-section-dark [number]
+  (board-section number "dark"))
+
+(defn- board-section-light [number]
+  (board-section number "light"))
+
+(def section-order (apply map list (partition 2 [20 1 18 4 13 6 10 15 2 17 3 19 7 16 8 11 14 9 12 5])))
 
 (defn board []
   "draw a board out of buttons"
   [:div {:class "board"}
-   (board-section 20)
-   (board-section 1)
-   (board-section 18)
-   (board-section 4)
-   (board-section 13)
-   (board-section 6)
-   (board-section 10)
-   (board-section 15)
-   (board-section 2)
-   (board-section 17)
-   (board-section 3)
-   (board-section 19)
-   (board-section 7)
-   (board-section 16)
-   (board-section 8)
-   (board-section 11)
-   (board-section 14)
-   (board-section 9)
-   (board-section 12)
-   (board-section 5)])
+   (map board-section-dark (first section-order))
+   (map board-section-light (second section-order))])
 
 (defn index [req]
   (layout/common req "TON EIGHTY" (board)))
